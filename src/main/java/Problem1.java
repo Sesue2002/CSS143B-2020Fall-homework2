f237e5649356c8e8e1f9d9fdf940d19b3aa5fd3a
Saul <sjpaguada@gmail.com>
2020-10-19 20:50:48 -0700

    problem 1 PABhfjewgvwoifvjed

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,18 +1,5 @@
 public class Problem1 {
 
-    /*
-    Describe your algorithm here in up to 5 sentences:
-    The algorithm used is a range method.
-    Each "color" has a certain range of low, mid, or high (written as for1s, etc..).
-    When we see a number, while traversing the array all in one go, we set it to a certain range.
-    After we set it to the correct range we update the amount in it for that range.
-    Using the switch statement, we update the array as we go.
-     */
-
-    /*Got some help from this website for how the function is written
-    https://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/
-    Do not change signature (function name, parameters type, and return type)
-     */
     public static void colorSort(int[] nums) {
         int for0s = 0;
         int for1s = 0;
